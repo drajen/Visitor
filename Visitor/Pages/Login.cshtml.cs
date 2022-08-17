a1523fd992ade817e0d2026771d050ba14821abd
Sean Milne <smilne@mandco.com>
2022-08-17 17:02:27 +0100

    Login page wierdness fixed

--- Visitor/Pages/Login.cshtml.cs
+++ Visitor/Pages/Login.cshtml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Collections.Generic;
@@ -8,13 +8,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
-namespace Visitor.Pages {
+namespace Visitor.Pages
+{
     [AllowAnonymous]
-    public class LoginModel : PageModel {
+    public class LoginModel : PageModel
+    {
         public string ReturnUrl { get; set; }
-        public async Task<IActionResult> OnGetAsync(string paramUsername, string paramPassword) {
+        public async Task<IActionResult> OnGetAsync(string paramUsername, string paramPassword)
+        {
             string returnUrl = Url.Content("~/admin");
-            try {
+            try
+            {
                 // Clear the existing external cookie
                 await HttpContext
                     .SignOutAsync(
@@ -23,23 +27,28 @@ namespace Visitor.Pages {
             catch { }
 
             // This is where you will validate the user
-            if (paramUsername == "admin" && paramPassword == "password1234") {
+            if (paramUsername == "admin" && paramPassword == "password1234")
+            {
                 var claims = new List<Claim> {
                     new Claim(ClaimTypes.Name, paramUsername),
                     new Claim(ClaimTypes.Role, "Administrator"),
                 };
                 var claimsIdentity = new ClaimsIdentity(
                     claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                var authProperties = new AuthenticationProperties {
+                var authProperties = new AuthenticationProperties
+                {
                     IsPersistent = true,
                     RedirectUri = this.Request.Host.Value
                 };
-                try {
+                try
+                {
                     await HttpContext.SignInAsync(
                     CookieAuthenticationDefaults.AuthenticationScheme,
                     new ClaimsPrincipal(claimsIdentity),
                     authProperties);
-                } catch (Exception ex) {
+                }
+                catch (Exception ex)
+                {
                     string error = ex.Message;
                 }
                 return LocalRedirect(returnUrl);
--- Visitor/Pages/Logout.cshtml.cs
+++ Visitor/Pages/Logout.cshtml.cs
@@ -5,9 +5,12 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 
-namespace Visitor.Pages {
-    public class LogoutModel : PageModel {
-        public async Task<IActionResult> OnGetAsync() {
+namespace Visitor.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGetAsync()
+        {
             // Clear the existing external cookie
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return LocalRedirect("/");
